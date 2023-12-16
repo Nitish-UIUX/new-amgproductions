db09951c6acba250b66d31e64aec9d7e40e9f91b
techgroupranchi03 <niteshdto17@gmail.com>
2023-12-17 01:03:34 +0530

    accout added

--- amg-production/src/Components/Header.jsx
+++ amg-production/src/Components/Header.jsx
@@ -1,72 +1,86 @@
 import React, { useState, useEffect } from 'react';
 import '../../src/index.css';
-import {NavLink} from 'react-router-dom'; 
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-  const [scrolled, setScrolled] = useState(false);
-  const [isOpen, setIsOpen] = useState(false); // State for menu toggle
+    const [scrolled, setScrolled] = useState(false);
+    const [isOpen, setIsOpen] = useState(false); // State for menu toggle
+    const [isAccountPopupOpen, setIsAccountPopupOpen] = useState(false); // State for account popup
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const offset = window.scrollY;
-      if (offset > 90) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
+    useEffect(() => {
+        const handleScroll = () => {
+            const offset = window.scrollY;
+            if (offset > 90) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
     };
 
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
+    const toggleAccountPopup = () => {
+        setIsAccountPopupOpen(!isAccountPopupOpen);
     };
-  }, []);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+    return (
+        <>
+            <nav className={scrolled ? 'navbar scrolled' : 'navbar'}>
+                <div className="navbar-container">
 
-  return (
-    <>
-    <nav className={scrolled ? 'navbar scrolled' : 'navbar'}>
-      <div className="navbar-container">
+                    <div className="navbar-logo">
+                        <NavLink exact to='/'>
+                            <img id='logo' src="images/logo.webp" alt="" />
+                        </NavLink>
+                    </div>
 
-        <div className="navbar-logo">
-          <NavLink exact to='/'>
-          <img id='logo' src="images/logo.webp" alt="" />
-          </NavLink>
-        </div>
+                    <div className={`navbar-menu ${isOpen ? 'active' : ''}`}>
 
-        <div className={`navbar-menu ${isOpen ? 'active' : ''}`}>
+                        <ul className="nav-links">
+                            <li><a href="/#home">Home</a></li>
+                            <li><a href="/#services">Services</a></li>
+                            <li><a href="/#portfolio">Portfolio</a></li>
+                            <li><a href="/#reviews">Reviews</a></li>
+                            <li><a href="/#team">Team</a></li>
+                            <li><a href="/#results">Results</a></li>
+                            <li><a href="/#about">About</a></li>
+                            <li><a href="/#contact">Contact</a></li>
+                        </ul>
 
-          <ul className="nav-links">
-            <li><a href="/#home">Home</a></li>
-            <li><a href="/#services">Services</a></li>
-            <li><a href="/#portfolio">Portfolio</a></li>
-            <li><a href="/#reviews">Reviews</a></li>
-            <li><a href="/#team">Team</a></li>
-            <li><a href="/#results">Results</a></li>
-            <li><a href="/#about">About</a></li>
-            <li><a href="/#contact">Contact</a></li>
-          </ul>
+                        <div id='navbar-icons'>
+                            <i className="ri-shopping-cart-2-line"></i>
+                            <i className="ri-account-circle-line" onClick={toggleAccountPopup}></i>
+                        </div>
 
-          <div id='navbar-icons'>
-              <i className="ri-shopping-cart-2-line"></i>
-              <i className="ri-account-circle-line"></i>
-          </div>
+                        <div className="menu-toggle" onClick={toggleMenu}>
+                            <i className={isOpen ? 'ri-close-fill' : 'ri-menu-fill'}></i>
+                        </div>
 
-          <div className="menu-toggle" onClick={toggleMenu}>
-            <i className={isOpen ? 'ri-close-fill' : 'ri-menu-fill'}></i>
-          </div>
+                    </div>
+                </div>
+            </nav>
 
-        </div>
-      </div>
-    </nav>
-  
-    </>
-  );
+            {isAccountPopupOpen && (
+                <div className="account-popup">
+                    {/* Content of your account popup goes here */}
+                    <div className="popup-content">
+                        <p>This is your account popup content.</p>
+                        {/* Add your account-related content here */}
+                    </div>
+                    <span className="close" onClick={toggleAccountPopup}>&times;</span>
+                </div>
+            )}
+        </>
+    );
 };
+
 export default Header;
-
-
